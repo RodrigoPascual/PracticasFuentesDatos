df65fefd29332e44d241d87fcf04b161498cb239
flooki10 <walidsabhi99@gmail.com>
2022-11-06 19:55:07 +0100

    SeminarioFuentes

--- SeminarioFuentes.R
+++ SeminarioFuentes.R
@@ -2,11 +2,24 @@
 library("dplyr")
 #vamos a hacer un join entre nuestras dos tablas por comunidad autonoma
 
+#
 combinacion <- inner_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
+#
+unionIzquierda <- left_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
+#
+unionDerecha <- right_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
+#
+unionTotal <- full_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
+#
+unionPracial <- semi_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
+#
+antiUnion <- anti_join(enfermedades,sedentarismo, by= "Comunidad autónoma")
 
 
 
 
 
 
+a <- as.numeric(enfermedades.Total)
 
+hist(x= enfermedades$as.numeric(Total), xlab = "Fctores", ylab = "Comunidad autonoma", main="Variacion de la tabla de enfermedades")
